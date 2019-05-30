fd73b7d44b9938afe4b0f682c571944521c8fd10
Harsh Joshi <joshih1998@gmail.com>
2019-05-30 08:16:47 +0530

    Updates

---
+++ precache-manifest.79ce96264957072219bcf647b345cd3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297a187514e418806d197c922346bd4f",
+    "url": "/plansreact/index.html"
+  },
+  {
+    "revision": "d0067ef9a5c81a0a5768",
+    "url": "/plansreact/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4181b43d06682eafdb17",
+    "url": "/plansreact/static/css/main.80e4a377.chunk.css"
+  },
+  {
+    "revision": "d0067ef9a5c81a0a5768",
+    "url": "/plansreact/static/js/2.23c60ab1.chunk.js"
+  },
+  {
+    "revision": "4181b43d06682eafdb17",
+    "url": "/plansreact/static/js/main.1a49da45.chunk.js"
+  },
+  {
+    "revision": "539d23aaba6b56ecc9e1",
+    "url": "/plansreact/static/js/runtime~main.88b3151f.js"
+  }
+]);
